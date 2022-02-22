d72ed8e7b4ca39f2b8ba856a57e5f1843b876eb5
Elithon Silva <elithonsilva@gmail.com>
2022-02-22 17:49:06 -0300

    remove interface de usuario

--- src/controllers/controllerUser.ts
+++ src/controllers/controllerUser.ts
@@ -1,8 +1,7 @@
 import dotenv from 'dotenv';
-
 import { Request, Response } from 'express';
-
 import * as jwt from 'jsonwebtoken';
+import interfacesUser from '../interfaces/interfacesUser';
 
 dotenv.config();
 
@@ -10,22 +9,15 @@ const authenticate = {
   mySecrete: String(process.env.JWT_SECRET),
 };
 
-interface Body {
-  username: string;
-  classe: string;
-  level: number;
-  password: string;
-}
-
 const createUser = async (req: Request, res: Response) => {
-  const { username }: Body = req.body;
+  const { username }: interfacesUser = req.body;
 
   const token = jwt.sign({ id: 1, username }, authenticate.mySecrete, {
     expiresIn: '12h',
     algorithm: 'HS256',
   });
 
-  res.status(201).json({ token });
+  return res.status(201).json({ token });
 };
 
 export default createUser;
