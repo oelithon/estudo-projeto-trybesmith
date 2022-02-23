23208485548cfd8ab3b39a6b52a38a349a82303a
Elithon Silva <elithonsilva@gmail.com>
2022-02-23 18:20:05 -0300

    impl func loginUser

---
+++ src/controllers/controllerLogin.ts
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+import { Request, Response } from 'express';
+import * as jwt from 'jsonwebtoken';
+import serviceUser from '../services/serviceUser';
+
+dotenv.config();
+
+const authenticate = {
+  mySecrete: String(process.env.JWT_SECRET),
+};
+
+const loginUser = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  const userSelected = await serviceUser.getUser({ username, password });
+
+  if (!userSelected) {
+    const message = { error: 'Username or password invalid' };
+    return res.status(401).json(message);
+  }
+  
+  const token = jwt.sign({ username }, authenticate.mySecrete, {
+    expiresIn: '12h',
+    algorithm: 'HS256',
+  });
+
+  return res.status(201).json({ token });
+};
+
+export default loginUser;
