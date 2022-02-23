091cb2278d1b31bd95ce56b74585f321d62beaf0
Elithon Silva <elithonsilva@gmail.com>
2022-02-23 14:49:31 -0300

    validado que o campo "username" é obrigatório

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import controllerUser from './controllers/controllerUser';
 
+import validateUsername from './middlewares/validateUser';
+
 const app = express();
 
 app.use(express.json());
 
 app
   .route('/users')
-  .post(controllerUser);
+  .post(
+    validateUsername,
+    controllerUser,
+  );
 
 export default app;
