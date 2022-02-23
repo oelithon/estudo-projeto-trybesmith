59ec87c915d24a2cf015f56418ced9af475ef858
Elithon Silva <elithonsilva@gmail.com>
2022-02-23 15:14:01 -0300

    impl validacao usernameString

--- src/middlewares/validateUser.ts
+++ src/middlewares/validateUser.ts
@@ -1,11 +1,18 @@
 import { NextFunction, Request, Response } from 'express';
 import interfacesUser from '../interfaces/interfacesUser';
 
-const validateUsername = (req: Request, res: Response, next: NextFunction) => {
+export const validateUsername = (req: Request, res: Response, next: NextFunction) => {
   const { username }: interfacesUser = req.body;
+  const message = { error: 'Username is required' };
 
-  if (!username) return res.status(400).json({ error: 'Username is required' });
+  if (!username) return res.status(400).json(message);
   next();
 };
 
-export default validateUsername;
+export const validateUsernameString = (req: Request, res: Response, next: NextFunction) => {
+  const { username }: interfacesUser = req.body;
+  const message = { error: 'Username must be a string' };
+
+  if (username !== typeof String) return res.status(422).json(message);
+  next();
+};
