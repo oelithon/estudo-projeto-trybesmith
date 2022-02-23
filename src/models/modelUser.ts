5f081a30049e2d2539a9ca8f245f071934c7a523
Elithon Silva <elithonsilva@gmail.com>
2022-02-23 17:02:46 -0300

    cria func getUser e import interfaceLogin

--- src/models/modelUser.ts
+++ src/models/modelUser.ts
@@ -1,5 +1,6 @@
 import connection from './connection';
 import interfacesUser from '../interfaces/interfacesUser';
+import interfaceLogin from '../interfaces/interfaceLogin';
 
 const create = async (user: interfacesUser) => {
   const { username, classe, level, password } = user;
@@ -11,4 +12,18 @@ const create = async (user: interfacesUser) => {
   );
 };
 
-export default create;
+const getUser = async (login: interfaceLogin) => {
+  const { username, password } = login;
+
+  const [users] = await connection.execute(`
+    SELECT * FROM Trybesmith.Users
+    WHERE username = ? AND password = ?
+    `, [username, password]);
+
+  return users;
+};
+
+export = {
+  create,
+  getUser,
+};
