99169cba68cbbadd253706b9d8115b6a27c166a3
Elithon Silva <elithonsilva@gmail.com>
2022-02-23 17:03:19 -0300

    impl func getUser e import interfaceLogin

--- src/services/serviceUser.ts
+++ src/services/serviceUser.ts
@@ -1,10 +1,21 @@
 import interfacesUser from '../interfaces/interfacesUser';
+import interfaceLogin from '../interfaces/interfaceLogin';
 import modelUser from '../models/modelUser';
 
 const create = async (user: interfacesUser) => {
   const { username, classe, level, password } = user;
 
-  await modelUser({ username, classe, level, password });
+  await modelUser.create({ username, classe, level, password });
 };
 
-export default create;
+const getUser = async (login: interfaceLogin) => {
+  const { username, password } = login;
+
+  const userSelected = await modelUser.getUser({ username, password });
+  return userSelected;
+};
+
+export = {
+  create,
+  getUser,
+};
