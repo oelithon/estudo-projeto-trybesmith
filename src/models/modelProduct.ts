832f17d9dd5c63ea3fa526d0f4343bc68392c63b
Elithon Silva <elithonsilva@gmail.com>
2022-02-24 11:42:50 -0300

    import ResultSetHeader

--- src/models/modelProduct.ts
+++ src/models/modelProduct.ts
@@ -1,14 +1,17 @@
+import { ResultSetHeader } from 'mysql2';
 import connection from './connection';
 import Product from '../interfaces/interfaceProduct';
 
 const create = async (product: Product) => {
   const { name, amount } = product;
-  await connection.execute(
+  const [result] = await connection.execute<ResultSetHeader>(
     `
     INSERT INTO Trybesmith.Products (name, amount)
     VALUES (?, ?)`,
     [name, amount],
   );
+
+  return result.insertId;
 };
 
 export default create;
